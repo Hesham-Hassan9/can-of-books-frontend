4756aed396824ec2bd9b07c2a94773018e0ce82b
Hesham-Hassan9 <19012748@student.ltuc.com>
2021-09-26 18:14:15 +0300

    book-frontend

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React from 'react';
-import Header from './Header';
-import IsLoadingAndError from './IsLoadingAndError';
-import Footer from './Footer';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React from "react";
+import Header from "./Header";
+import IsLoadingAndError from "./IsLoadingAndError";
+import Footer from "./Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { withAuth0 } from "@auth0/auth0-react";
+import Login from "./Login";
+import BestBooks from "./BestBooks.js";
+import Profile from "./Profile.js";
 
 class App extends React.Component {
-
   render() {
-    console.log('app', this.props);
-    return(
+    console.log("app", this.props);
+    return (
       <>
         <Router>
           {/* <IsLoadingAndError> */}
-            <Header />
-            <Switch>
-              <Route exact path="/">
-                {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
-              </Route>
-              {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
-            </Switch>
-            <Footer />
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              {
+                /* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */
+                !this.props.auth0.isAuthenticated ? <Login/>:  <BestBooks/>
+              }
+            </Route>
+            {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+            <Route path="/profile" component={Profile}/>
+          </Switch>
+          <Footer />
           {/* </IsLoadingAndError> */}
         </Router>
       </>
@@ -31,4 +34,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default withAuth0 (App);
