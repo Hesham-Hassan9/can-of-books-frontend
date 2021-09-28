a50970a256492249daffe8276cd4b1549ebd8756
Hesham-Hassan9 <19012748@student.ltuc.com>
2021-09-28 12:52:27 +0300

    solving all problems

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Header";
-import IsLoadingAndError from "./IsLoadingAndError";
+// import IsLoadingAndError from "./IsLoadingAndError";
 import Footer from "./Footer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { withAuth0 } from "@auth0/auth0-react";
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,43 +1,39 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Jumbotron from "react-bootstrap/Jumbotron";
 import "./BestBooks.css";
 import axios from "axios";
+import Book from "./component/Book.js";
 import { withAuth0 } from "@auth0/auth0-react";
-import Card from 'react-bootstrap/Card';
-import { ListGroup } from "react-bootstrap";
 
 class MyFavoriteBooks extends React.Component {
-  componentDidMount = async () => {
-    const { getIdTokenClaims } = this.props.auth0;
-    let tokenClaims = await getIdTokenClaims();
-    const jwt = tokenClaims.__raw;
-
-    const config = {
-      headers: { Authorization: `Bearer ${jwt}` },
+  constructor(props) {
+    super(props);
+    this.state = {
+      ArrayOfbook: [],
+      searchQuery: "",
     };
-    const bookData = await axios.get(
-      `${process.env.REACT_APP_BACKEND_URL}/books`,
-      config
-    );
+  }
+
+  componentDidMount = async () => {
+    let email = this.props.auth0.user.email;
+
+    let bookUrl = `http://localhost:3001/getBook?email=${email}`;
+    let bookData = await axios.get(bookUrl);
+    console.log("hi");
     console.log(bookData);
-    this.setState({ bookData: bookData.data });
+    this.setState({
+      ArrayOfbook: bookData.data,
+    });
   };
+
   render() {
-    const { user } = this.props.auth0;
     return (
-      <Jumbotron>
-        <h1>My Favorite Books</h1>
-        <h3>This is a collection of {user.name} favorite books</h3>
-        <Card style={{ width: "18rem" }}>
-          <Card.Header>Featured</Card.Header>
-          <ListGroup variant="flush">
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
-            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-          </ListGroup>
-        </Card>
-      </Jumbotron>
+      <div>
+        <h1>My Fav Books</h1>
+        {this.state.ArrayOfbook.map((data, ind) => {
+          return <Book ArrayOfbook={data} index={ind} />;
+        })}
+      </div>
     );
   }
 }
